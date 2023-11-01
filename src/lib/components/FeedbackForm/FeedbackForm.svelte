0f6b88902d8ac8efd214d0a1e4252bd3de9a7114
Lasse Wolpmann <lassewolpmann@outlook.de>
2023-11-01 14:53:28 +0200

    Changed look of feedback form

--- src/lib/components/FeedbackForm/FeedbackForm.svelte
+++ src/lib/components/FeedbackForm/FeedbackForm.svelte
@@ -5,7 +5,7 @@
 </script>
 <style lang="scss">
     .feedback-form {
-        position: absolute;
+        position: fixed;
         bottom: 0;
         right: 0;
         margin: 20px;
@@ -18,42 +18,57 @@
         gap: 15px;
 
         button {
-            width: 60px;
-            height: 60px;
             font-size: 24px;
         }
 
-        div {
+        .form {
             display: none;
-            height: min-content;
-            width: min-content;
-            background: var(--table-row-primary-color);
-
-            border-radius: 10px;
-
-            overflow-y: scroll;
-
-            padding: 20px;
-        }
-
-        div.expanded {
-            display: flex;
-            flex-direction: row;
+            flex-direction: column;
             align-items: center;
             justify-content: center;
 
-            gap: 10px;
+            background: var(--table-row-primary-color);
+
+            border: var(--border);
+            border-radius: 10px;
+
+            padding: 10px;
+        }
+
+        .form.expanded {
+            display: flex;
+
+            .buttons {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+
+                gap: 10px;
+                padding-bottom: 10px;
+
+                button:first-child {
+                    color: green;
+                }
+
+                button:last-child {
+                    color: red;
+                }
+            }
         }
     }
 </style>
 <div class="feedback-form">
     <button on:click={() => formExpanded = !formExpanded}><i class="fa-solid fa-message-smile"></i></button>
-    <div class:expanded={formExpanded}>
+    <div class:expanded={formExpanded} class="form">
         {#if !hasGivenFeedback}
-            <button on:click={() => hasGivenFeedback = true} class="plausible-event-name=Good"><i class="fa-solid fa-thumbs-up"></i></button>
-            <button on:click={() => hasGivenFeedback = true} class="plausible-event-name=Bad"><i class="fa-solid fa-thumbs-down"></i></button>
+            <h3>Would you recommend this website?</h3>
+            <div class="buttons">
+                <button on:click={() => hasGivenFeedback = true} class="plausible-event-name=Good"><i class="fa-solid fa-thumbs-up"></i></button>
+                <button on:click={() => hasGivenFeedback = true} class="plausible-event-name=Bad"><i class="fa-solid fa-thumbs-down"></i></button>
+            </div>
         {:else}
-            <p>Thanks!</p>
+            <p>Thank you for your feedback!</p>
         {/if}
     </div>
 </div>
