59f1a498d592f79c50009b75248dd1a564dd678c
Lasse Wolpmann <lassewolpmann@outlook.de>
2023-11-01 14:08:52 +0200

    Modified imports

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,18 @@
 import type { Actions } from './$types';
-import { Client } from "pg";
+
+import pg from 'pg';
+const client = new pg.Client({
+    host: POSTGRES_HOST,
+    port: +POSTGRES_PORT,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+});
+
 import { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } from '$env/static/private';
 
 export const actions = {
     default: async ({ request }) => {
-        const client = new Client({
-            host: POSTGRES_HOST,
-            port: +POSTGRES_PORT,
-            database: POSTGRES_DB,
-            user: POSTGRES_USER,
-            password: POSTGRES_PASSWORD
-        });
-
         try {
             await client.connect();
 
