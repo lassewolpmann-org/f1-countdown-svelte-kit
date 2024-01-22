bebde1cb791d91a26e039e36ae3d94ffa8f4fe54
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-01-22 09:26:48 +0200

    Added Instagram Post for McLaren Car Launch

--- src/routes/car-launches/+server.ts
+++ src/routes/car-launches/+server.ts
@@ -12,7 +12,7 @@ export const GET: RequestHandler = () => {
         {team_name: "Ferrari", location: "TBC", tbc: false, date: "2024-02-13T12:00:00Z", announcement: "https://www.instagram.com/p/C1AFL2VqvoN/"},
         {team_name: "Red Bull", location: "TBC", tbc: false, date: "2024-02-15T12:00:00Z", announcement: "https://www.instagram.com/p/C2IH88JP_Ph/"},
         {team_name: "Mercedes", location: "Silverstone", tbc: false, date: "2024-02-14T12:00:00Z", announcement: "https://www.instagram.com/p/C2HXajQtJV-/"},
-        {team_name: "McLaren", location: "Silverstone", tbc: false, date: "2024-02-14T12:00:00Z", announcement: "https://www.instagram.com/mclaren/"},
+        {team_name: "McLaren", location: "Silverstone", tbc: false, date: "2024-02-14T12:00:00Z", announcement: "https://www.instagram.com/p/C2XrWcDCEWW/"},
         {team_name: "Alpine", location: "TBC", tbc: false, date: "2024-02-07T12:00:00Z", announcement: "https://www.instagram.com/p/C2HXe_4t1Sq/"},
         {team_name: "AlphaTauri", location: "TBC", tbc: true, date: "", announcement: ""},
         {team_name: "Haas", location: "Silverstone", tbc: false, date: "2024-02-11T12:00:00Z", announcement: "https://www.instagram.com/haasf1team/"}
