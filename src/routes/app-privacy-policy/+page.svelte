7df4e9ba40cc0f02ff494245b0c7acaa056b09a9
Lasse Wolpmann <33729366+lassewolpmann@users.noreply.github.com>
2024-01-30 12:44:30 +0200

    Added Privacy Policy for iOS App

---
+++ src/routes/app-privacy-policy/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    // Font imports
+    import "@fontsource/poppins/100.css";
+    import "@fontsource/poppins/200.css";
+    import "@fontsource/poppins/300.css";
+    import "@fontsource/poppins/400.css";
+    import "@fontsource/poppins/500.css";
+    import "@fontsource/poppins/600.css";
+    import "@fontsource/poppins/700.css";
+    import "@fontsource/poppins/800.css";
+</script>
+
+<style>
+    section {
+        font-family: 'Poppins', sans-serif;
+    }
+</style>
+
+<svelte:head>
+    <title>App Privacy Policy</title>
+</svelte:head>
+<a href="/"><i class="fa-solid fa-left"></i> Back to website</a>
+
+<section>
+    <h1>Privacy Policy for Formula Countdown</h1>
+    <p>Last updated: 30.01.2024</p>
+</section>
+
+<section>
+    <h1>Introduction</h1>
+    <p>
+        Welcome to Formula Countdown ("we," "our," or "us").
+        This Privacy Policy is intended to inform you about the collection, use, and protection of your information
+        when you use our mobile application and services ("App").
+    </p>
+    <p>
+        By accessing or using the App, you agree to the terms of this Privacy Policy.
+        If you do not agree with the terms of this Privacy Policy, please do not use the App.
+    </p>
+</section>
+
+<section>
+    <h1>1. Information We Collect</h1>
+    <p>
+        Apple Analytics: Our App utilizes Apple's analytics services to collect anonymized and aggregated information
+        about App usage, performance, and user engagement. This information helps us understand how users interact
+        with the App and improve its functionality.
+    </p>
+</section>
+
+<section>
+    <h1>2. Use of Information</h1>
+    <p>
+        We use the information collected through Apple Analytics for the sole purpose of improving the App's
+        performance, user experience, and overall functionality. We do not collect or process any third-party data,
+        and we do not use location services.
+    </p>
+</section>
+
+<section>
+    <h1>3. Security</h1>
+    <p>
+        We take reasonable measures to protect the information collected through Apple Analytics from unauthorized
+        access or disclosure. However, no method of transmission over the internet or electronic storage is completely
+        secure. Therefore, we cannot guarantee absolute security.
+    </p>
+</section>
+
+<section>
+    <h1>4. Your Choices</h1>
+    <p>
+        As the App does not collect personal information directly and only utilizes Apple Analytics, you have limited
+        options for data control. You can manage your device settings related to the App and control the sharing of
+        analytics data with Apple.
+    </p>
+</section>
+
+<section>
+    <h1>5. Changes to this Privacy Policy</h1>
+    <p>
+        We may update this Privacy Policy periodically. The latest version will be posted on the App,
+        and the effective date will be indicated.
+    </p>
+</section>
+
+<section>
+    <h1>6. Contact Us</h1>
+    <p>
+        If you have any questions or concerns about this Privacy Policy, please contact us at:
+        <a href="mailto:lasse@f1-countdown.com">lasse@f1-countdown.com</a>
+    </p>
+</section>
