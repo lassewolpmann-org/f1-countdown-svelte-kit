df39e0d5ea555efa68bc33c2850dc784dcabcdf9
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-04-16 19:50:17 +0300

    Added Support Route for App Store Submission

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    // Font imports
+    import "@fontsource/poppins/100.css";
+    import "@fontsource/poppins/200.css";
+    import "@fontsource/poppins/300.css";
+    import "@fontsource/poppins/400.css";
+    import "@fontsource/poppins/500.css";
+    import "@fontsource/poppins/600.css";
+    import "@fontsource/poppins/700.css";
+    import "@fontsource/poppins/800.css";
+</script>
+
+<style>
+    section {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    a {
+        text-decoration: underline;
+    }
+</style>
+
+<svelte:head>
+    <title>Support</title>
+</svelte:head>
+
+<section>
+    <h1>Support</h1>
+    <p><i class="fa-solid fa-envelope"></i> You can contact us <a href="mailto:lasse©f1-countdown.com">here</a> over e-mail.</p>
+    <p><i class="fa-brands fa-github"></i> Or create an issue on GitHub <a href="https://github.com/lassewolpmann-org/f1-countdown-svelte-kit/issues" target="_blank"> here.</a></p>
+</section>
